c0caf0c0bd12f93ba7f8e378dce9258536b56f30
mushonnip <mushonnipabu@gmail.com>
2022-01-22 20:34:46 +0700

    revert config

--- next.config.js
+++ next.config.js
@@ -3,24 +3,4 @@ module.exports = {
   env: {
     NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
